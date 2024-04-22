31a685762e7ecd9d5430009a839288ee161786b6
anajembaedwin <anajembaedwin@gmail.com>
2024-04-22 07:28:13 +0100

    updated headers and footers

--- src/data/user.js
+++ src/data/user.js
@@ -16,21 +16,20 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Backend and DevOps Engineer, with interest in Data and Machine Learning.",
+		title: "Shopify Developer, Backend and DevOps Engineer, with interest in Data and Machine Learning.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a software developer with expertise in backend development, shopify development, and devOps, using HTML, CSS, React, Node.js, Python, ASP.NET, Go and Bash. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Edwin Anajemba. I live in Abuja, where I develop solutions for the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	"articles": {
+		"title": "Driven by Innovation: Inspiring Future Generations",
+		"description": "A sequential anthology of my in-depth reflections on software development, leadership dynamics, and the solutions found in data."
 	},
 
 	projects: [
