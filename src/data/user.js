14eb15e734fbc00ed075fd75c1eb089062f5fd3d
Edwin Anajemba <anajembaedwin@gmail.com>
2024-07-19 00:55:20 +0100

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -16,9 +16,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Shopify Developer, Backend and DevOps Engineer, with interest in Data and Machine Learning.",
+		title: "Blockchain Developer, Backend and DevOps Engineer, with interest in Data and Machine Learning.",
 		description:
-			"I am a software developer with expertise in backend development, shopify development, and devOps, using HTML, CSS, React, Node.js, Python, ASP.NET, Go and Bash. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a software developer with expertise in blockchain development, backend development, and devOps, using HTML, CSS, React, Node.js, Python, Solidity, Hardhat, web3.js, Go and Bash. I have experience in building scalable, secure and reliable smart contracts and web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	"about": {
