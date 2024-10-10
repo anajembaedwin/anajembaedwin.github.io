82eb7d312f934b57a08648a386d5932853174263
anajembaedwin <anajembaedwin@gmail.com>
2024-10-10 16:51:35 +0100

    updated projects

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -16,79 +16,76 @@ const Works = () => {
 						<div className="work">
 							<img
 								src="./brief-case.jpg"
-								alt="facebook"
+								alt="Netzence Sustainability Limited"
 								className="work-image"
 							/>
-							<div className="work-title">Propatidotnet</div>
-							<div className="work-subtitle">
-								Backend and DevOps Engineer
+							<div>
+								<div className="work-title">Netzence Sustainability Limited</div>
+								<div className="work-subtitle">
+									Full Stack Web Developer (E-Commerce, Payment Integration, Web3)
+								</div>
 							</div>
-							<div className="work-duration">2024 - Present</div>
+							<div className="work-duration">Jun 2024 - Present</div>
 						</div>
 
 						<div className="work">
 							<img
 								src="./brief-case.jpg"
-								alt="twitter"
+								alt="Algorinth Labs LLC"
 								className="work-image"
 							/>
-							<div className="work-title">Algorinth Labs LLC</div>
-							<div className="work-subtitle">
-								Backend and Smart Contract Engineer
+							<div>
+								<div className="work-title">Algorinth Labs LLC</div>
+								<div className="work-subtitle">
+									Full Stack Developer
+								</div>
 							</div>
-							<div className="work-duration">2023 - 2024</div>
+							<div className="work-duration">Jul 2023 - Jun 2024</div>
 						</div>
 
 						<div className="work">
 							<img
 								src="./brief-case.jpg"
-								alt="facebook"
+								alt="Grandida LLC"
 								className="work-image"
 							/>
-							<div className="work-title">Grandida LLC</div>
-							<div className="work-subtitle">
-								Backend and Smart Contract Engineer
+							<div>
+								<div className="work-title">Grandida LLC</div>
+								<div className="work-subtitle">
+									Full Stack Developer
+								</div>
 							</div>
-							<div className="work-duration">2022 - 2023</div>
+							<div className="work-duration">Jul 2021 - Jun 2023</div>
 						</div>
 
 						<div className="work">
 							<img
 								src="./brief-case.jpg"
-								alt="facebook"
+								alt="Palimart LLC"
 								className="work-image"
 							/>
-							<div className="work-title">Palimart LLC</div>
-							<div className="work-subtitle">
-								Fullstack Developer - Contract
+							<div>
+								<div className="work-title">Palimart LLC</div>
+								<div className="work-subtitle">
+									Full Stack Developer (Frontend Heavy)
+								</div>
 							</div>
-							<div className="work-duration">2021 - 2022</div>
+							<div className="work-duration">Apr 2020 - May 2021</div>
 						</div>
 
 						<div className="work">
 							<img
 								src="./brief-case.jpg"
-								alt="facebook"
+								alt="Prince Ebeano & Co."
 								className="work-image"
 							/>
-							<div className="work-title">UCPro LLC</div>
-							<div className="work-subtitle">
-								Junior Web Developer - Contract 
+							<div>
+								<div className="work-title">Prince Ebeano & Co.</div>
+								<div className="work-subtitle">
+									Inventory Analyst
+								</div>
 							</div>
-							<div className="work-duration">2020 - 2021</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./brief-case.jpg"
-								alt="facebook"
-								className="work-image"
-							/>
-							<div className="work-title">STEM Teers NGO</div>
-							<div className="work-subtitle">
-								Junior Frontend Developer - Internship 
-							</div>
-							<div className="work-duration">2018 - 2020</div>
+							<div className="work-duration">Mar 2018 - Apr 2020</div>
 						</div>
 					</div>
 				}
--- src/data/user.js
+++ src/data/user.js
@@ -16,9 +16,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Blockchain Developer, Backend and DevOps Engineer, with interest in Data and Machine Learning.",
+		title: "Full Stack Developer, Blockchain, and DevOps Engineer, with Interest in Data and Machine Learning.",
 		description:
-			"I am a software developer with expertise in blockchain development, backend development, and devOps, using HTML, CSS, React, Node.js, Python, Solidity, Hardhat, web3.js, Go and Bash. I have experience in building scalable, secure and reliable smart contracts and web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a Full Stack Developer with a strong foundation in blockchain development, backend engineering, and DevOps. Proficient in HTML, CSS, React, Node.js, Python, Solidity, Hardhat, web3.js, Go, and Bash, I specialize in creating scalable, secure, and reliable web applications and smart contracts. My experience spans various frameworks and technologies, allowing me to build solutions that adhere to industry best practices and standards. I thrive on solving complex problems, continually learning new skills, and embracing new challenges to enhance my development capabilities.",
 	},
 
 	"about": {
@@ -33,48 +33,93 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			title: "Buz and Socials Reservations",
+			description: "Customer facing page.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/anajembaedwin/buz-and-socials-reservations",
+			technologies: "JavaScript",
+			updatedDate: "Aug 27"
 		},
-
 		{
-			title: "Project 2",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			title: "Buz and Socials Hotel Booking Dashboard",
+			description: "Hotel Booking Dashboard.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/hotel-booking-dashboard",
+			technologies: "JavaScript",
+			updatedDate: "Aug 26"
+		},
+		{
+			title: "Backend for the hotel-booking-dashboard",
+			description: "Backend for the hotel-booking-dashboard.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/hotel-booking-backend",
+			technologies: "JavaScript",
+			updatedDate: "Aug 25"
+		},
+		{
+			title: "Portfolio Website for Edwin Anajemba",
+			description: "Portfolio website to showcase my work.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/anajembaedwin.github.io",
+			technologies: "JavaScript",
+			updatedDate: "Aug 19"
+		},
+		{
+			title: "Smartworka Frontend",
+			description: "Smartworka is a productivity tool to manage a project from inception.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/smartworka-frontend",
+			technologies: "TypeScript",
+			updatedDate: "Jun 24"
+		},
+		{
+			title: "Ecommerce Storefront for a Fullstack Medusa App",
+			description: "Ecommerce storefront built using Medusa.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/fullstack-medusa-app-storefront",
+			technologies: "TypeScript",
+			updatedDate: "Apr 29"
+		},
+		{
+			title: "Vintage Jazz Records API",
+			description: "A RESTful API developed with Go and Gin for Vintage Jazz Records.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/go/go.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/vintage-jazz-records",
+		},
+		{
+			title: "Palimart Cosmetics Backend",
+			description: "A NodeJS and Express RESTful API Backend for Palimart Cosmetics.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/anajembaedwin/cosmetics_backend",
+		},
+		{
+			title: "Travel and Tours Business Backend",
+			description: "A backend for a travel and tours business built with Python, Django, and PostgreSQL.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/anajembaedwin/travels_backend",
 		},
-
 		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			title: "Simple Flask REST API",
+			description: "A simple Flask REST API capable of CRUD operations on a 'person' resource.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/anajembaedwin/simple-flask-rest-api",
 		},
-
 		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			title: "Personal Blog",
+			description: "A personal blog for writing about web development and technologies.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/anajembaedwin/personal-blog",
 		},
 	],
 };
